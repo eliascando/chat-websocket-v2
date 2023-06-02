59e74ad4c77a66916506e94158c0662e09554b90
Elias Esteban Cando Jaime <elias.candoj@ug.edu.ec>
2023-06-02 16:59:40 -0500

    feat: manejo de chats por salas

--- src/index.js
+++ src/index.js
@@ -10,29 +10,55 @@ const io = require('socket.io')(server, {
 const rooms= {};
 
 io.on('connection', (socket) => {
-    console.log('\nse conecto un cliente')
-    socket.on('join_room',(data) =>{
-        const { room , user} = data;
+    socket.on('join_room', (data) => {
+        const { room, user } = data;
         socket.join(room);
-        if(!rooms[room]){
-            rooms[room] = [];
+      
+        if (!rooms[room]) {
+          rooms[room] = [];
         }
-        rooms[room].push(user);
-        console.log(`${user} en sala ${room}`)
-        console.log(rooms);
-        const message = {
-            usuario: 'INFO',    
-            mensaje: `${user} se ha unido a la sala`
-        };
-        io.to(room).emit('chat_message', message)
+      
+        if (!rooms[room].includes(user)) {
+            rooms[room].push(user);
+            const message = {
+              usuario: 'INFO',
+              mensaje: `${user} se ha unido a la sala`
+            };
+            io.to(room).emit('chat_message', message);
+        }
     });
     
     socket.on('chat_message', (data) => {  
         const { room, message } = data;
         const { usuario, mensaje } = message
         io.to(room).emit('chat_message', message)
-        console.log(data) 
     });
+
+    socket.on('exit_room', (data) => {
+        const { user, room } = data;
+      
+        // Verificar si el usuario está en la sala
+        if (rooms[room] && rooms[room].includes(user)) {
+          // Remover al usuario de la lista de usuarios de la sala
+          rooms[room] = rooms[room].filter(username => username !== user);
+      
+          // Emitir un mensaje informando la desconexión del usuario
+          const message = {
+            usuario: 'INFO',
+            mensaje: `${user} se ha desconectado de la sala ${room}`
+          };
+          io.to(room).emit('chat_message', message);
+      
+          socket.leave(room);
+          
+          if (rooms[room].length === 0) {
+              // Eliminar la sala
+              delete rooms[room];
+            }
+        }
+      });
+      
+      
 })
 
 server.listen(port, () => {
